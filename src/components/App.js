0bd178a76667d896b462f2c956fc5b7b4f4b00ad
Annisa <annisakaraca@gmail.com>
2018-04-03 17:36:34 -0700

    Replace React components with container components

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import VideoListContainer from '../containers/VideoListContainer.js';
 import VideoPlayerContainer from '../containers/VideoPlayerContainer.js';
+import SearchContainer from '../containers/SearchContainer';
 import Nav from './Nav.js';
 import VideoPlayer from './VideoPlayer.js';
 import VideoList from './VideoList.js';
@@ -46,15 +47,13 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <Nav handleSearchInputChange={this.getYouTubeVideos.bind(this)}/>
+        <SearchContainer />
         <div className="row">
           <div className="col-md-7">
-            <VideoPlayer video={this.state.currentVideo}/>
+            <VideoPlayerContainer />
           </div>
           <div className="col-md-5">
-            <VideoList
-              handleVideoListEntryTitleClick={this.handleVideoListEntryTitleClick.bind(this)}
-              videos={this.state.videos}
+            <VideoListContainer
             />
           </div>
         </div>
--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -11,7 +11,7 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    handleSearchInputChange: () => dispatch(handleSearchChange())
+    handleSearchInputChange: (query) => dispatch(handleSearchChange(query))
   };
 };
 
